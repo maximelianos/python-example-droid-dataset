41401fd67b2ab139905232e30728c344b5c3254d
Max Argus <argus.max@gmail.com>
2025-02-04 18:05:41 +0100

    visual filtered

--- play.py
+++ play.py
@@ -1,12 +1,18 @@
-import json
+# import json
+#
+# data = {"school": 1543}
+# print(json.dumps(data))
+#
+# # file line reading
+# with open("tmp.txt", "w") as f:
+#     print("str1", file=f)
+#     print("str2", file=f)
+#
+# with open("tmp.txt", "r") as f:
+#     print([line.strip() for line in f.readlines()])
+#
 
-data = {"school": 1543}
-print(json.dumps(data))
 
-# file line reading
-with open("tmp.txt", "w") as f:
-    print("str1", file=f)
-    print("str2", file=f)
+import numpy as np
 
-with open("tmp.txt", "r") as f:
-    print([line.strip() for line in f.readlines()])
+print(np.clip([-10, 20], 0, 1))
